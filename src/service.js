84dabe4034468949857f82354889dedb4515749e
qsa2020 <>
2020-07-13 19:53:28 +0800

    权限修改

--- src/service.js
+++ src/service.js
@@ -1,7 +1,7 @@
 class Service {
   constructor() {
     this.isTest = true
-    this.isLocal = false
+    this.isLocal = true
     if (this.isLocal) {
       this.http = 'http'
       this.host = 'localhost'
